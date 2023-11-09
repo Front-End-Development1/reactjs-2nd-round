c386d6d706d352364c94ab800ce4e4fabfc3cec0
Loang_khan <solangisohailkhan903@gmail.com>
2023-11-10 01:44:14 +0500

    routing completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import './App.css'
+import Header from './components/Header';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div>
-     <h1>Hello world</h1>
-    </div>
+   <BrowserRouter>
+     
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+   </BrowserRouter>
   );
 }
 
