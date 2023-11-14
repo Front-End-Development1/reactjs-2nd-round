8acb544a4c5abe768892e217baec9774d1d755f1
Loang_khan <solangisohailkhan903@gmail.com>
2023-11-14 15:55:23 +0500

    form handling and validation completed using formik and yub library

--- src/formDataHandling/FormData.jsx
+++ src/formDataHandling/FormData.jsx
@@ -1,43 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik, Formik, Form, Field } from 'formik';
+import { signupValidation } from './SignUpValidation';
+
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    cpassword: '',
+}
 
 function FormData() {
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-});
 
-const handleInputChange = (e) => {
-    const {name, value } = e.target;
-    setFormData({
-        ...formData,
-        [name]: value,
-    });
-};
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted:');
-};
 
 return (
-    <div>
-        <h2>Form Data Handling</h2>
-        <firm onSubmit={handleSubmit}>
-            <label>
-                Username: <input type='text' name='username' value={formData.username} />
-            </label>
-            <br />
-            <label>
-               Email: <input type='email' name='email' value={formData.email} onChange={handleInputChange} />
-            </label>
-            <br />
-            <label>
-                Password: <input type='password' name='password' value={formData.password} onChange={handleInputChange} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </firm>
+    <div className='form'>
+        <Formik
+        initialValues={initialValues}
+        validationSchema={signupValidation}
+        
+        >
+
+            {({errors}) => (
+                <Form>
+                <label htmlFor='name'>Name</label>
+                <br />
+                <Field type="text" name="name"></Field>
+                <br />
+                {errors.name && <small>{errors.name}</small>}
+                <br />
+                <label htmlFor='email'>Email</label>
+                <br />
+                <Field type="email" name="name"></Field>
+                <br />
+                {errors.email && <small>{errors.email}</small>}
+                <br />
+                <label htmlFor='password'>Password</label>
+                <br />
+                <Field type="password" name="password"></Field>
+                <br />
+                {errors.password && <small>{errors.password}</small>}
+                <br />
+                <label htmlFor='cpassword'>Confrim Password</label>
+                <br />
+                <Field type="cpassword" name="cpassword"></Field>
+                <br />
+                {errors.cpassword && <small>{errors.cpassword}</small>}
+                <br />
+                <button type='submit'>Submit</button>
+               </Form>
+            )}
+        
+        </Formik>
+       
     </div>
 );
 }
